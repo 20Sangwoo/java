93c1412e314ae719a1bca9a2b69637796cf5552f
20Sangwoo <144245121+20Sangwoo@users.noreply.github.com>
2023-10-25 17:03:15 +0900

    Add files via upload

---
+++ week05/report/CircleManager2.java
@@ -0,0 +1,44 @@
+package reports5;
+import java.util.Scanner;
+
+class Circle2 {
+	   private double x, y;
+	   private int radius;
+	   public Circle2(double x, double y, int radius) {
+	      this.x = x;
+	      this.y =y;
+	      this.radius = radius;
+	   }
+	   public void show() {
+	      System.out.println("(" + x + "," + y + ")"+ radius);
+	   }
+	   public int getRadius() {
+	      return radius;
+	   }
+	}
+
+	public class CircleManager2 {
+
+		 public static void main(String[] args) {
+		      // TODO Auto-generated method stub
+		      Scanner scanner = new Scanner(System.in);
+		      Circle2 c[] = new Circle2[3]; 
+		      int max=0; 
+		      for(int i=0; i < c.length; i ++) {
+		         System.out.print("x, y, radius >>");
+		         double x = scanner.nextDouble();
+		         double y = scanner.nextDouble();
+		         int radius = scanner.nextInt(); 
+		         c[i] = new Circle2(x, y, radius); 
+		      }
+		      for(int i=0; i<c.length; i++) 
+		         if(max < c[i].getRadius())
+		            max = c[i].getRadius();
+		      for(int i=0; i<c.length; i++)
+		         if(max == c[i].getRadius()) {
+		            System.out.print("가장 면적인 큰 원은 ");
+		            c[i].show();
+		         }
+		      scanner.close();
+		   }
+		}
---
+++ week05/report/Tv.java
@@ -0,0 +1,25 @@
+package reports5;
+
+class InfoTV {
+	 private String brand;
+	   private int year;
+	   private int inch;
+	   InfoTV(String brand, int year, int inch) {
+	      this.brand = brand;
+	      this.year = year;
+	      this.inch = inch;
+	   }
+	   public void show() {
+	      System.out.println(brand+"에서 만든 "+year+"년형 "+inch+"인치 TV");
+	   }
+	}
+
+	public class Tv {
+
+	   public static void main(String[] args) {
+	      // TODO Auto-generated method stub
+	      InfoTV myTV = new InfoTV("LG", 2017, 32);
+	      myTV.show();
+	   }
+
+	}
